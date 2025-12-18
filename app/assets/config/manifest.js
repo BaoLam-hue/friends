844c4f5ee07ef86e92dc2ddc28251e9922e7a6c1
BaoLam2K3 <60538673+BaoLam2K3@users.noreply.github.com>
2025-12-18 15:35:22 +0700

    add a bunch of unversioned files
    Signed-off-by: BaoLam2K3 <60538673+BaoLam2K3@users.noreply.github.com>

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css//= link_tree ../builds
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
